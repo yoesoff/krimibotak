fef5e2df043c6cc7f1cb627ae10f58d45d11fec7
Yusuf GAE <mhyusufibrahim@gmail.com>
2017-12-07 17:37:12 +0700

    Second commit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 
 var firebase = require('firebase');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World Happy Express day!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
